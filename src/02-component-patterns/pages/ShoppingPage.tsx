3523c814a96a394704bd953b6feb6c69fcf74a06
soyl3y3nd4 <pacors88@gmail.com>
2022-04-10 06:13:20 +0200

    pre npm rdy

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -3,8 +3,6 @@ import { ProductImage, ProductTitle, ProductButtons } from '../components';
 
 import { products } from "../data/products";
 
-import '../styles/custom-styles.css';
-
 const product = products[0];
 
 export const ShoppingPage = () => {
@@ -16,25 +14,17 @@ export const ShoppingPage = () => {
       <ProductCard
         key={product.id}
         product={product}
-        className="bg-dark text-white"
         initialValues={{
           count: 4,
-          maxCount: 10
+          // maxCount: 10
         }}
       >
         {
           ({ reset, increaseBy, isMaxCountReached, counter, maxCount }) => (
             <>
-              <ProductImage className="custom-image" />
-              <ProductTitle className="text-bold" />
-              <ProductButtons className="custom-buttons" />
-              <button onClick={reset}>Reset</button>
-              <button onClick={() => increaseBy(-2)}>-2</button>
-              {
-                !isMaxCountReached && <button onClick={() => increaseBy(2)} >+2</button>
-              }
-
-              <span>{counter} - {maxCount}</span>
+              <ProductImage />
+              <ProductTitle />
+              <ProductButtons />
             </>
           )
         }
